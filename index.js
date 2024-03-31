4e3f08d93b8e5bafacdf2aa039407d4dc77a18d5
mahesh <maheshshastrakar123@gmail.com>
2024-03-31 22:43:51 +0530

    updated

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
-db.on('error',(err) => console.error(err));
-db.once('open',() => console.log("Database Connected"));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("<h1>Welcome to Simple REST API </h1> <br>Please go to the /data endpoint for further communication");
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Welcome to Simple REST API </h1> <br>Please go to the /data endpoint for further communication"
+  );
 });
 
-const firstRouter = require('./routes/data');
-app.use('/data',firstRouter);
-
+const firstRouter = require("./routes/data");
+app.use("/data", firstRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server Started at http://localhost:${process.env.PORT}`);
+  console.log(`Server Started at http://localhost:${process.env.PORT}`);
 });
--- routes/data.js
+++ routes/data.js
@@ -1,73 +1,67 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Data = require('../models/data.model');
+const Data = require("../models/data.model");
 
 // get all
-router.get('/', async (req,res)=>{
-    try {
-        const data = await Data.find();
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({message : error.message});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const data = await Data.find();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // get one
-router.get('/:id',getData,(req,res)=>{
-    res.json(req.data);
+router.get("/:id", getData, (req, res) => {
+  res.json(req.data);
 });
 
 // post data
-router.post('/', async (req,res)=>{
-    const data = new Data({
-        name: req.body.name,
-        age: req.body.age
-    })
-    try {
-        const newData = await data.save();
-        res.status(201).json(newData);
-    } catch (err) {
-        res.status(400).json({message : err.message});
-    }
+router.post("/", async (req, res) => {
+  const data = new Data({
+    name: req.body.name,
+    age: req.body.age,
+  });
+  try {
+    const newData = await data.save();
+    res.status(201).json(newData);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // update one
-router.patch('/:id',getData,async (req,res)=>{
-    if(req.body.name!=null){
-        req.data.name = req.body.name;
-    }
-    if(req.body.age!=null){
-        req.data.age = req.body.age;
-    }
-    try {
-        const updatedData = await req.data.save();
-        res.status(200).json(updatedData);
-    } catch (error) {
-        res.status(400).json({message : error.message});
-    }
+router.patch("/:id", getData, async (req, res) => {
+  try {
+    const updatedData = await req.data.save();
+    res.status(200).json(updatedData);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 // delete one
-router.delete('/:id',getData, async (req,res)=>{
-    try {
-        await req.data.deleteOne();
-        res.json({message:"Data Erased"});
-    } catch (error) {
-        res.json({message:error.message})
-    }
+router.delete("/:id", getData, async (req, res) => {
+  try {
+    await req.data.deleteOne();
+    res.json({ message: "Data Erased" });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 });
 
-async function getData(req,res,next){
-    let data;
-    try {
-        data = await Data.findById(req.params.id);
-        if(data == null){
-            res.status(404).json({"message" : "Data not found"});
-        }
-    } catch (error) {
-        res.status(500).json({message: error.message});
+async function getData(req, res, next) {
+  let data;
+  try {
+    data = await Data.findById(req.params.id);
+    if (data == null) {
+      res.status(404).json({ message: "Data not found" });
     }
-    req.data = data;
-    next()
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  req.data = data;
+  next();
 }
 
 module.exports = router;
